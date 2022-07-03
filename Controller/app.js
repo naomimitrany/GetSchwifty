736fc187bcbc3b8ab535be47f472d8f2e1794e01
Naomi Mitrany <naomi.mitrany@gmail.com>
2022-07-03 19:55:09 +0300

    Basic board

--- Controller/app.js
+++ Controller/app.js
@@ -1,3 +1,20 @@
 function generateBoard() {
-    prompt("Enter board size:", 4)
+    let boardSize = prompt("Enter board size:", 4);
+    let body = document.body;
+    body.innerHTML = "";
+    let board = document.createElement("div");
+    board.className = "board";
+    let boardPrecent = `${100 / boardSize}% `.repeat(boardSize);
+    board.style.gridTemplateColumns = boardPrecent;
+    board.style.gridTemplateRows = boardPrecent;
+
+    let items = [];
+    for (let i = 1; i <= boardSize * boardSize - 1; i++) {
+        items[i] = document.createElement("div");
+        items[i].className = "board-item";
+        items[i].textContent = i;
+        board.appendChild(items[i])
+    }
+
+    body.appendChild(board);
 }
